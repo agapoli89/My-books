809e2388600666b23b174b18c969319c5a60e0f9
Agnieszka Polińska <agapoli@gmail.com>
2021-06-13 12:28:05 +0200

    Implement List component

---
+++ src/store/List.jsx
@@ -0,0 +1,21 @@
+import { connect } from 'react-redux';
+
+import Element from './Element';
+
+const List = ({rates}) => {
+    const ratesElements = rates.map(rate => <Element key={rate.id} {...rate}/>)
+
+    return (  
+        <ul>
+            {ratesElements}
+        </ul>
+    );
+}
+
+const connectReduxStateToProps = store => ({
+    rates: store.rates,
+})
+ 
+const ListConsumer = connect(connectReduxStateToProps)(List);
+
+export default ListConsumer;
