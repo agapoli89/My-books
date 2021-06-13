e3c5523ec26e4b4ee3b6559d821c8362760021ed
Agnieszka Polińska <agapoli@gmail.com>
2021-06-13 12:28:41 +0200

    Add title and List component to the app

--- src/App.js
+++ src/App.js
@@ -2,15 +2,16 @@ import { Provider } from 'react-redux';
 
 import store from './store/store';
 import Form from './Form';
+import List from './store/List';
 
 import './style.css';
 
 function App() {
   return (
     <Provider store={store}>
-      <div>
-        <Form />
-      </div>
+      <h3>Redux</h3>
+      <Form />
+      <List />
     </Provider>
   );
 }
