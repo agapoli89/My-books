ed344f654aa9fa1dad79ff70677ff8eaf0a22660
Agnieszka Polińska <agapoli@gmail.com>
2021-06-17 08:31:02 +0200

    Refactor code
    use hook useDispatch

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,23 +1,23 @@
 import { useState } from 'react';
 
-import { connect } from 'react-redux';
+import { useDispatch } from 'react-redux';
 
 import { addRate, editRate } from './actions/appActions';
 
 import './styles/Form.css'
 
 const Form = ({
-    addRate,
     author = '',
     callback,
     comment = '',
-    editRate,
     id,
     rate= 5,
 }) => {
     const [authorInput, setAuthorInput] = useState(author);
     const [rateInput, setRateInput] = useState(rate);
     const [commentInput, setCommentInput] = useState(comment);
+
+    const dispatch = useDispatch();
 
     const handleOnChangeAuthorInput = e => setAuthorInput(e.target.value);
     const handleOnChangeRateInput = e => setRateInput(e.target.value);
@@ -32,7 +32,9 @@ const Form = ({
             rate: Number(rateInput),
         };
 
-        id ? editRate(rateObject) : addRate(rateObject);
+        id 
+            ? dispatch(editRate(rateObject)) 
+            : dispatch(addRate(rateObject));
 
         if (id) {
             callback();
@@ -71,11 +73,4 @@ const Form = ({
     );
 }
 
-const connectActionsToProps = ({
-    addRate,
-    editRate,
-})
-
-const FormConsumer = connect(null, connectActionsToProps)(Form);
-
-export default FormConsumer;
+export default Form;
--- src/middleware/validateAuthorMiddleware.js
+++ src/middleware/validateAuthorMiddleware.js
@@ -3,7 +3,7 @@ import { ADD } from '../actions/appActions';
 export const validateAuthorMiddleware = store => next => action => {
     console.log(action);
 
-    if (action.type === ADD && !action.payload.author.lenght) {
+    if (action.type === ADD && !action.payload.author.length) {
         console.warn('Proszę wpisz autora')
         return;
     }
